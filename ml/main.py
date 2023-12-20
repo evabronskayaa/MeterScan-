e8d2e903a2136b4381f434643dda7d281408cdd3
LuckyValenok <luckyvalenok1@gmail.com>
2023-12-20 19:14:02 +0500

    Возвращен requirements.txt и мл записывает результаты напрямую в бд

--- ml/main.py
+++ ml/main.py
@@ -2,10 +2,14 @@ import os
 import sys
 import json
 
+import grpc
 import requests
 import pika
 from PIL import Image
 
+from ml.proto.database_pb2 import UpdateFullPredictionRequest
+from ml.proto.database_pb2_grpc import DatabaseServiceStub
+from ml.proto.image_pb2 import RecognitionResult, Scope
 from text_detection import detect_objects_on_image, process_ocr_result, concat_all_results
 
 USER = 'user'
@@ -14,8 +18,7 @@ BROKER_HOSTNAME = 'rabbitmq'
 PORT = 5672
 QUEUE_NAME = 'predictions'
 
-WEB_HOSTNAME = 'web'
-WEB_PORT = '8080'
+DATABASE = ''
 
 CONNECTION_URL = f'amqp://{USER}:{PASSWORD}@{BROKER_HOSTNAME}:{PORT}/'
 
@@ -23,24 +26,27 @@ CONNECTION_URL = f'amqp://{USER}:{PASSWORD}@{BROKER_HOSTNAME}:{PORT}/'
 def handle_message(ch, method, properties, body):
     body_str = body.decode('utf-8')
     request = json.loads(body_str)
-    
+
     index = request.get('index')
     image_url = request.get('image')
 
-    image_data = Image.open(requests.get(image_url, stream = True).raw)
+    image_data = Image.open(requests.get(image_url, stream=True).raw)
 
     contours = detect_objects_on_image(image_data)
     ocr_result = process_ocr_result()
-    concat_results = concat_all_results(contours, ocr_result)
-    
-    payload = {
-        'index': index,
-        'results': concat_results
-    }
 
-    payload_json = json.dumps(payload)
+    pb2_results = []
+    for result in concat_all_results(contours, ocr_result):
+        pb2_result = RecognitionResult(recognition=result[0],
+                                       metric=result[1],
+                                       scope=Scope(
+                                           x1=result[2][0], y1=result[2][1],
+                                           x2=result[2][2], y2=result[2][3]))
+        pb2_results.append(pb2_result)
 
-    requests.post(f'http://{WEB_HOSTNAME}:{WEB_PORT}/predictions/', data=payload_json)
+    with grpc.insecure_channel(DATABASE) as channel:
+        stub = DatabaseServiceStub(channel)
+        stub.UpdateFullPrediction(UpdateFullPredictionRequest(id=index, results=pb2_results))
 
 
 def main():
