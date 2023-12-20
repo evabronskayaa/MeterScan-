c9aad5206f65d54d9cd3a3a9aef71d436de3cfb3
LuckyValenok <luckyvalenok1@gmail.com>
2023-12-21 00:20:11 +0500

    Nginx

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/v1/";
+const API_URL = "http://localhost/api/v1/";
 
 class AuthService {
   login(email, password, recaptcha) {
